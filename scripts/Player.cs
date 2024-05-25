05077ad5b6501213d7e31af6a8f8e7002eff8a2f
Nicola Stroffolino <91049774+nicola-stroffolino@users.noreply.github.com>
2024-05-25 12:42:20 +0200

    editing some sprites

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -3,17 +3,13 @@ using System;
 
 public partial class Player : CharacterBody3D {
 	public AnimatedSprite3D Sprite { get; set; }
-	public Vector2 InputDirection { get; set; }
+	public Vector2 SpriteDirection { get; set; } // should NEVER be equal to Vector2.Zero
 
 	public const float Speed = 5.0f;
-	public const float JumpVelocity = 4.5f;
-
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
 
     public override void _Ready() {
 		Sprite = GetNode<AnimatedSprite3D>("%Sprite");
-		InputDirection = Vector2.Zero;
+		SpriteDirection = Vector2.Down;
 	}
 
     public override void _Process(double delta) {
@@ -21,55 +17,34 @@ public partial class Player : CharacterBody3D {
     }
 
     public override void _PhysicsProcess(double delta) {
-
-		// // Add the gravity.
-		// if (!IsOnFloor())
-		// 	velocity.Y -= gravity * (float)delta;
-
-		// // Handle Jump.
-		// if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		// 	velocity.Y = JumpVelocity;
-
-		// // Get the input direction and handle the movement/deceleration.
-		// // As good practice, you should replace UI actions with custom gameplay actions.
-		// Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		// Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-
 		MoveAndSlide();
 	}
 
     public override void _Input(InputEvent @event) {
 		if (@event is not InputEventKey) return;
 
-		InputDirection = new Vector2 {
+		var inputDirection = new Vector2 {
 			X = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left"),
 			Y = Input.GetActionStrength("move_down") - Input.GetActionStrength("move_up")
 		};
 
-		if (InputDirection == Vector2.Zero) return;
+		if (inputDirection != Vector2.Zero) {
+			SpriteDirection = inputDirection;
+			Sprite.Play("walk_" + Game.Directions[SpriteDirection]);
+		}
+		else Sprite.Play("idle_" + Game.Directions[SpriteDirection]);
 
-		// int frame = 0;
-		// switch(InputDirection) {
-		// 	case (0, -1): frame = 0; break;
-		// 	case (1, -1): frame = 1; break;
-		// 	case (1, 0): frame = 2; break;
-		// 	case (1, 1): frame = 3; break;
-		// 	case (0, 1): frame = 4; break;
-		// 	case (-1, 1): frame = 5; break;
-		// 	case (-1, 0): frame = 6; break;
-		// 	case (-1, -1): frame = 7; break;
-		// }
-		// Sprite.FrameCoords = new Vector2I(frame, Sprite.FrameCoords.Y);
+		inputDirection = inputDirection.Normalized();
 
-		InputDirection = InputDirection.Normalized();
 		Vector3 velocity = Velocity;
-		if (InputDirection != Vector2.Zero) {
-			velocity.X = InputDirection.X * Speed;
-			velocity.Z = InputDirection.Y * Speed;
+		if (inputDirection != Vector2.Zero) {
+			velocity.X = inputDirection.X * Speed;
+			velocity.Z = inputDirection.Y * Speed;
 		} else {
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
 		}
+
 		Velocity = velocity;
     }
 }
--- singletons/Game.cs
+++ singletons/Game.cs
@@ -3,10 +3,13 @@ using Godot.Collections;
 
 public partial class Game : Node {
     public static Dictionary<Vector2, string> Directions { get; set; } = new() {
-        
+        { Vector2.Up, "u" },
+        { Vector2.Down, "d" },
+        { Vector2.Left, "l" },
+        { Vector2.Right, "r" },
+        { Vector2.Up + Vector2.Right, "ur" },
+        { Vector2.Up + Vector2.Left, "ul" },
+        { Vector2.Down + Vector2.Right, "dr" },
+        { Vector2.Down + Vector2.Left, "dl" },
     };
 }
-
-// public enum Directions {
-    
-// }
